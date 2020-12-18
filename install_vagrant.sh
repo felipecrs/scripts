91178d757f030068a2e9df80817a72a916a86b97
Felipe Santos <felipecassiors@gmail.com>
2020-12-18 12:41:58 -0300

    Fix vagrant script not taking latest version

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-VAGRANT_VERSION="$(wget -q -O- https://releases.hashicorp.com/vagrant/ | fgrep 'href="/vagrant' | head -1 | sed -r 's/.*([0-9].[0-9].[0-9]).*/\1/')"
-wget -q "https://releases.hashicorp.com/vagrant/$VAGRANT_VERSION/vagrant_${VAGRANT_VERSION}_x86_64.deb"
-sudo apt-get install -qq ./vagrant_${VAGRANT_VERSION}_x86_64.deb
-rm ./vagrant_${VAGRANT_VERSION}_x86_64.deb
+VAGRANT_VERSION="$(wget -q -O- https://releases.hashicorp.com/vagrant/ | fgrep 'href="/vagrant' | head -1 | sed -r 's/.*([0-9]+.[0-9]+.[0-9]+).*/\1/')"
+wget -q "https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb"
+sudo apt-get install -y "./vagrant_${VAGRANT_VERSION}_x86_64.deb"
+rm -f "./vagrant_${VAGRANT_VERSION}_x86_64.deb"
